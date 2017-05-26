b9e5fbad6fc2167cafe68ad1c2fccd23bcb7266b
huliang@dongyin.com <hu7486232>
2017-05-26 10:09:25 +0800

    第一次提交

---
+++ app/src/main/java/com/dy/testgit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dy.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试git，提交，更新，解决冲突
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
